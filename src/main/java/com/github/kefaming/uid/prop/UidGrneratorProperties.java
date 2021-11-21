fd0640c025fc30f44ea653c6bf37bb0dbcb7b705
kefaming <kefaming@163.com>
2021-11-22 00:17:44 +0800

    代码改造

---
+++ src/main/java/com/github/kefaming/uid/prop/UidGrneratorProperties.java
@@ -0,0 +1,9 @@
+//package com.github.kefaming.uid.prop;
+//
+//import org.springframework.boot.context.properties.ConfigurationProperties;
+//
+//@ConfigurationProperties(prefix = "uid")
+//public class UidGrneratorProperties {
+//
+//
+//}
