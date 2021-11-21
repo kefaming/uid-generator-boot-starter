97c4a9d36ed9ad35db8e331450fe3883cf51b2ea
kefaming <kefaming@163.com>
2021-11-22 00:17:26 +0800

    代码改造

--- src/main/java/com/example/demo/mapper/StorageMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.example.demo.model.Storage;
-import org.apache.ibatis.annotations.Param;
-
-public interface StorageMapper extends BaseMapper<Storage> {
-
-}
